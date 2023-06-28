fdfcd7995ad2b2c8b6216c33047ca59df0366dd6
owenloh <101535709+owenloh@users.noreply.github.com>
2023-06-28 14:40:40 +0100

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private Button GetButton1()
+        {
+            return button1;
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent(Button button1)
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(68, 87);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(662, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(349, 177);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "decrypt";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += this.button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(316, 296);
+            label1.Name = "label1";
+            label1.Size = new Size(156, 20);
+            label1.TabIndex = 2;
+            label1.Text = "output: decrypted text";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(785, 365);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "\\';";
+            Load += this.Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(GetButton1());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void button1_Click(object sender, System.EventArgs e)
+        {
+            label1.Text = Juniper.Decrypt9(textBox1.Text);
+        }
+    }
+
+    class Juniper
+    {
+        private static Dictionary<char, int> SALT_LEN = new();
+        private static List<char> NUM_ALPHA = new();
+        private static Dictionary<char, int> ALPHA_NUM = new();
+
+        static List<List<int>> ENCODING = new List<List<int>>
+        {
+            new List<int> { 1, 4, 32 },
+            new List<int> { 1, 16, 32 },
+            new List<int> { 1, 8, 32 },
+            new List<int> { 1, 64 },
+            new List<int> { 1, 32 },
+            new List<int> { 1, 4, 16, 128 },
+            new List<int> { 1, 32, 64 }
+        };
+
+        private static void Decrypt9_setup()
+        {
+            string[] FAMILY = { "QzF3n6/9CAtpu0O", "B1IREhcSyrleKvMW8LXx", "7N-dVbwsY2g4oaJZGUDj", "iHkq.mPf5T" };
+
+            int x = 0;
+            foreach (var foo in FAMILY)
+            {
+                foreach (var bar in foo)
+                {
+                    SALT_LEN[bar] = 3 - x;
+                    NUM_ALPHA.Add(bar); //FAMILY concatinated
+                }
+                x++;
+            }
+
+            for (int y = 0; y < NUM_ALPHA.Count; y++)
+            {
+                ALPHA_NUM[NUM_ALPHA[y]] = y;
+            }
+        }
+
+        public static string Decrypt9(string crypt)
+        {   // the crypt is of the form $9$ + salt_len_indicator + salt + hash
+
+            if (crypt.Substring(0, 3) != "$9$") return "Error: incorrect format"; //making sure the crypt is the correct form/type
+            Decrypt9_setup();
+
+            string salt_n_hash = crypt.Split(new[] { "$9$" }, StringSplitOptions.None)[1]; //removing $9$
+            char salt_len_indicator = salt_n_hash[0]; 
+            string hash = salt_n_hash.Substring(SALT_LEN[salt_len_indicator]+1); //removing the salt
+
+            // decrypting the hash
+            char prev = salt_len_indicator;
+            string decrypt = "";
+            while (hash.Length > 0)
+            {
+                var decode = ENCODING[decrypt.Length % 7];
+                string nibble = hash.Substring(0, decode.Count); //each nibble will result in a character
+                hash = hash.Substring(decode.Count); // removing the nibble from the hash
+                List<int> gaps = new();
+                foreach (char i in nibble)
+                {
+                    int g = Gap(prev, i);
+                    prev = i;
+                    gaps.Add(g);
+                }
+                decrypt += (char) GapDecode(gaps, decode);
+            }
+
+            return decrypt;
+        }
+
+
+        static int Gap(char c1, char c2)
+        {
+            return ((ALPHA_NUM[c2] - ALPHA_NUM[c1]) % NUM_ALPHA.Count) - 1;
+        }
+
+        static char GapDecode(List<int> gaps, List<int> dec)
+        {
+            int num = 0;
+            if (gaps.Count != dec.Count)
+            {
+                Console.WriteLine("Nibble and decode size not the same!");
+                Environment.Exit(1);
+            }
+            for (int i = 0; i < gaps.Count; i++)
+            {
+                num += (gaps[i]*dec[i]);
+            }
+
+            int ASCII = num % 256;
+            System.Diagnostics.Debug.WriteLine(ASCII.ToString());
+            return (char)(ASCII);
+        }
+    }
+}
